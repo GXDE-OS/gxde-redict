db3740c632a76ef12db2225514c062b592fc40f9
shenmo <jifengshenmo@outlook.com>
2020-08-08 08:36:20 +0800

    改包名

---
+++ moc_popupcontent.cpp
@@ -0,0 +1,115 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'popupcontent.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.11.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "popup/popupcontent.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'popupcontent.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.11.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_PopupContent_t {
+    QByteArrayData data[4];
+    char stringdata0[80];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_PopupContent_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_PopupContent_t qt_meta_stringdata_PopupContent = {
+    {
+QT_MOC_LITERAL(0, 0, 12), // "PopupContent"
+QT_MOC_LITERAL(1, 13, 13), // "updateContent"
+QT_MOC_LITERAL(2, 27, 0), // ""
+QT_MOC_LITERAL(3, 28, 51) // "std::tuple<QString,QString,QS..."
+
+    },
+    "PopupContent\0updateContent\0\0"
+    "std::tuple<QString,QString,QString,QString,QString>"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_PopupContent[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    1,   19,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 3,    2,
+
+       0        // eod
+};
+
+void PopupContent::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        PopupContent *_t = static_cast<PopupContent *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->updateContent((*reinterpret_cast< std::tuple<QString,QString,QString,QString,QString>(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject PopupContent::staticMetaObject = {
+    { &DAbstractDialog::staticMetaObject, qt_meta_stringdata_PopupContent.data,
+      qt_meta_data_PopupContent,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *PopupContent::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *PopupContent::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_PopupContent.stringdata0))
+        return static_cast<void*>(this);
+    return DAbstractDialog::qt_metacast(_clname);
+}
+
+int PopupContent::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = DAbstractDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 1;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
